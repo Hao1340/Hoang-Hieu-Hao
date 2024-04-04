a6ac0f8f000e41f9a041013be76733f786ebf6db
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-04 23:25:04 +0700

    add cousrse code

---
+++ Models/Courses.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SMS.Models
+{
+    public class Courses
+    {
+        private int CoursesId;
+        public string CoursesName;
+
+        public int Courseid
+        {
+            get { return CoursesId; }
+            set { CoursesId = value; }
+        }
+    }
+}
